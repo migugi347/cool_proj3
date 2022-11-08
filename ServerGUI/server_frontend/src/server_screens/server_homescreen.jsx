58ca8d0b3b1c54dd87e17c99ad13a6bfe053dde6
Jose Antonio Real <antonio@main-10-230-20-96.tamulink.tamu.edu>
2022-11-08 02:42:54 -0600

    Added order summary section that includes option to add customer's name, see lists of items ordered, get total cost of order, and buttons to submit/cancel order

--- ServerGUI/server_frontend/src/server_screens/server_homescreen.jsx
+++ ServerGUI/server_frontend/src/server_screens/server_homescreen.jsx
@@ -13,9 +13,37 @@ function Server_homescreen() {
 
     return (
         <body className = "homeScreen">
-            {menuItems.map((val) => (
-                <button>{val.Name}</button>
-            ))}
+            <div className = "menuItems">
+                {menuItems.map((val) => (
+                    <button>{val.Name}</button>
+                ))}
+            </div>
+            <div className = "orderSummary">
+                    <p contentEditable = "true" className = "customerName">Enter Customer's Name:</p>
+                   <table>
+                        <thead>
+                            <tr>
+                                <th>Quantity</th>
+                                <th>Menu Item</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Chocloate</td>
+                                <td>$5.00</td>
+                            </tr>
+                        </tbody>
+                    </table> 
+                    <p className = "totalCost">Total Cost:</p>
+                    <div className = "submitButton">
+                        <button>Submit Order</button>
+                    </div>
+                    <div className = "cancelButton">
+                        <button>Cancel Order</button>
+                    </div>
+            </div>
         </body>
   );
 }

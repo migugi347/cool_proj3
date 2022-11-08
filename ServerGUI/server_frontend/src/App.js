f90e48519d9b4f5ffa81cbb2d6ad93b9ab2b673c
Jose Antonio Real <antonio@main-10-230-20-96.tamulink.tamu.edu>
2022-11-08 09:49:45 -0600

    Added code such that order summary section appears on all screens

--- ServerGUI/server_frontend/src/App.js
+++ ServerGUI/server_frontend/src/App.js
@@ -6,11 +6,13 @@ import{
 import ServerHomeScreen from './server_screens/server_homescreen';
 import ServerCategoriesScreen from './server_screens/server_categoryitemsscreen';
 import ServerNavigationBar from './server_navigationBar';
+import ServerOrderSummary from './server_orderSummary';
 
 function App() {  
   return(
     <>
       <ServerNavigationBar/>
+      <ServerOrderSummary/>
       <Routes>
         <Route path = "/" element = {<ServerHomeScreen/>} />
         <Route path = "/:category" element = {<ServerCategoriesScreen/>} />
--- ServerGUI/server_frontend/src/server_screens/server_homescreen.jsx
+++ ServerGUI/server_frontend/src/server_screens/server_homescreen.jsx
@@ -18,32 +18,6 @@ function Server_homescreen() {
                     <button>{val.Name}</button>
                 ))}
             </div>
-            <div className = "orderSummary">
-                    <p contentEditable = "true" className = "customerName">Enter Customer's Name:</p>
-                   <table>
-                        <thead>
-                            <tr>
-                                <th>Quantity</th>
-                                <th>Menu Item</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Chocloate</td>
-                                <td>$5.00</td>
-                            </tr>
-                        </tbody>
-                    </table> 
-                    <p className = "totalCost">Total Cost:</p>
-                    <div className = "submitButton">
-                        <button>Submit Order</button>
-                    </div>
-                    <div className = "cancelButton">
-                        <button>Cancel Order</button>
-                    </div>
-            </div>
         </body>
   );
 }

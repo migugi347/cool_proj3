fe4a74b06c430191f31620e9ae5beae7229a5c71
husein654 <husein654@tamu.edu>
2022-11-20 20:05:34 -0600

    body color picker and translator implemented

--- frontend/src/layouts/Mainlayout.jsx
+++ frontend/src/layouts/Mainlayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState,useEffect} from "react";
 import { Link } from 'react-router-dom'
 import Logo from './images/SB_logo.png';
+import { ChromePicker } from 'react-color'
 import "../style.scss"
 
-function mainlayout({ children }) {
+function Mainlayout({ children }) {
     // const [account, setAccount] = useState([]);
     // useEffect(() => {
     //     const loggedInUser = localStorage.getItem("user");
@@ -12,6 +13,27 @@ function mainlayout({ children }) {
     //         setAccount(foundUser);
     //     }
     // }, []);
+
+    const google = window.google;
+    useEffect(() => {
+        var addScript = document.createElement('script');
+        addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');
+        document.body.appendChild(addScript);
+        window.googleTranslateElementInit = googleTranslateElementInit;
+    }, [])
+
+    const googleTranslateElementInit = () => {
+        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+    }
+
+
+    const [color, setColor] = useState('#fff')
+    const [showColorPicker, setShowColorPicker] = useState(false)
+
+    const colorchange = (newcolor) =>{
+        let root = document.querySelector('body');
+        root.style.setProperty('background-color',newcolor);
+    };
 
     return (
         <div>
@@ -22,8 +44,17 @@ function mainlayout({ children }) {
                             <img style={{ width: "15%", height: "15%" }} src={Logo} alt="starbucks_logo" />
                             <Link to="/" className="navbar-brand text-light" style={{ fontWeight: 800 }}>    STARBUCKS</Link>
                         </div>
-                        <div >   </div>
+                        <div > <div id="google_translate_element"></div>  </div>
                     </div>
+                    <button className='btn btn-secondary' onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}>
+                        {showColorPicker ? 'Close' : 'Pick a color'}
+                    </button>
+                    {showColorPicker && (
+                        <ChromePicker
+                        color={color}
+                        onChange={updatedColor => colorchange(updatedColor.hex)}
+                        />
+                    )}
                 </nav>
             </header>
             <main>
@@ -44,4 +75,4 @@ function mainlayout({ children }) {
     )
 }
 
-export default mainlayout
+export default Mainlayout
